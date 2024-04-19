974587327dd5ce0935fffb32e91ccc47a67a937f
kceeyang <155762876+kceeyang@users.noreply.github.com>
2024-04-19 15:27:09 -0500

    chalk w5 change

--- DigitalChalkboard/js/w5chalk.js
+++ DigitalChalkboard/js/w5chalk.js
@@ -6,11 +6,11 @@ function checkTriviaAnswer()
 
     if(answer.toLowerCase() === correctAnswer.toLowerCase())
     {
-        responseElement.textContent = `Correct! You gussed: ${answer}`;
+        responseElement.textContent = `Correct! The answer is: ${answer}`;
     }
     else
     {
-        responseElement.textContent = `Incorrect! You gussed: ${answer}`;
+        responseElement.textContent = `Incorrect! The answer is NOT: ${answer}`;
     }
 }
 
@@ -32,24 +32,24 @@ function checkNumber()
 
 
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//     const triviaInput = document.getElementById("trivia-submit");
-//     triviaInput.addEventListener("keypress", function(event){
-//         if (event.key === "Enter")
-//         {
-//             event.preventDefault();
-//             checkTriviaAnswer();
-//         }   
-//     });
-//     const numberInput = document.getElementById("numberCheck");
-//     numberInput.addEventListener("keypress", function(event){
-//         if (event.key === "Return")
-//         {
-//             event.preventDefault();
-//             checkNumber();
-//         }   
-//     });
+    const triviaInput = document.getElementById("trivia-answer");
+    triviaInput.addEventListener("keypress", function(event){
+        if (event.key === "Enter")
+        {
+            event.preventDefault();
+            checkTriviaAnswer();
+        }   
+    });
+    const numberInput = document.getElementById("number-input");
+    numberInput.addEventListener("keypress", function(event){
+        if (event.key === "Enter")
+        {
+            event.preventDefault();
+            checkNumber();
+        }   
+    });
 
-// });
+});
 
