39602a13fdd70492f4b017a31090603f62a11a11
kceeyang <155762876+kceeyang@users.noreply.github.com>
2024-04-20 16:47:31 -0500

    chalk w5 change

--- DigitalChalkboard/js/w5chalk.js
+++ DigitalChalkboard/js/w5chalk.js
@@ -14,18 +14,31 @@ function checkTriviaAnswer()
     }
 }
 
+function ltrim(x, characters) {
+    var start = 0;
+    while (characters.indexOf(x[start]) >= 0) {
+      start += 1;
+    }
+    var end = x.length - 1;
+    return x.substr(start);
+  }
+
+
 function checkNumber()
 {
     const numberInput = document.getElementById("number-input").value;
     const num = parseInt(numberInput);
-    if(!isNaN(num) && num>= 10000 && num <= 99999)
+
+    if(!isNaN(num) && num>= 10000 && num <= 99999 && numberInput.length==5)
     {
         const isEven = num % 2 === 0;
         document.getElementById("number-response").innerText = `The number ${num} is ${isEven ? "even" : "odd"}.`;
+        console.log(numberInput)   //check numberInput length to handle the leading zeros problem (eg. '0099999')
     }
     else
     {
         document.getElementById("number-response").innerText = "Please enter a valid 5-digit number.";
+        console.log(numberInput)
     }
 }
 
