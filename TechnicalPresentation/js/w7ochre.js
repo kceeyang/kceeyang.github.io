2f24f00f8a653089690625aae3976ef3be7238dd
kceeyang <155762876+kceeyang@users.noreply.github.com>
2024-05-04 19:45:54 -0500

    technical w7 update

--- TechnicalPresentation/js/w7ochre.js
+++ TechnicalPresentation/js/w7ochre.js
@@ -1,38 +1,98 @@
-// // getXml(fetch_id)
-// //     .then(xml)
-// //     .then(add_uuid => {
-// //         if(add_uuid){
-// //             return getXml(add_uuid)
-// //                 .then(xml)
+// version with XHR
+document.addEventListener('DOMContentLoaded', () => {
+    const uuid = document.getElementById('ochreContainer').getAttribute('data-uuid');
+    const ochre_url = "https://ochre.lib.uchicago.edu/ochre?uuid=";
+    const link = ochre_url + uuid;
 
-// //         }
-// //     })
-// //     .catch(error => {
-// //         display.innerHTML = "Error" + error.message;
-// //     });
+    loadXML();
+
+    // load the XML
+    function loadXML()
+    {
+      requestXML(link, {redirect: 'follow'});
+      console.log('loadXML -- OK');
+    }
+
+    // make an Ochre API call using an xhr object
+    function requestXML(link) 
+    {
+      var connect = new XMLHttpRequest();
+      connect.onreadystatechange = function()
+      {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+          parseXML(this.responseXML);  // passes the requested xml file to the parseXML function 
+        }
+      };
+      connect.open("GET", link, true);
+      connect.send();
+      console.log('requestXML -- OK');
+    };
+
+    // get title, properties, and desired images from Ochre xml
+    function parseXML(sourceXML) 
+    {
+
+      // get title of item for the xml page
+      var textTitle = sourceXML.getElementsByTagName('identification');
+      var title_string = document.createTextNode(textTitle[1].textContent);
+      document.getElementById('title').appendChild(title_string);
 
 
+      // get properties of item for the xml page
+      if (sourceXML.getElementsByTagName('property').length > 1)
+      {
+        const properties = sourceXML.querySelectorAll('property');
+        i = 0;
+        properties.forEach(p => {
+          var tr = document.createElement('tr');
+          tr.setAttribute('class', 'ochreTableRows');
+          tr.setAttribute('id', 'row_' + i);
+          document.getElementById('ochreTableBody').appendChild(tr);
+
+          var property = document.createElement('td');
+          property.setAttribute('id', 'property_' + i);
+          property.textContent = p.querySelector('string').textContent;
+          document.getElementById('row_' + i).appendChild(property);
+
+          var value = document.createElement('td');
+          value.setAttribute('id', 'property_value_' + i);
+          value.textContent = p.querySelector('value').textContent;
+          document.getElementById('row_' + i).appendChild(value);
+          i++;
+        });
+      }
+      
+
+      // get preview image of item for the xml page
+      if (sourceXML.getElementsByTagName('resource')[0].getAttribute("format") == 'image/jpeg')
+      {
+        var img = document.createElement('img');
+        var src = link + "&preview";
+        img.src = src;
+        document.getElementById('preview').appendChild(img);
+      }
+
+    };
+
+});
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// // version with fetch() - for the 1st page
 // document.addEventListener('DOMContentLoaded', function() {
-//     const fetch_id = document.body.getAttribute('data-uuid');
+//     const fetch_id = document.getElementById('ochreContainer').getAttribute('data-uuid');
 //     const ochre_url = 'https://ochre.lib.uchicago.edu/ochre?uuid=';
-//     var display = document.getElementById('called-container');
+//     var display = document.getElementById('ochreTableBody');
 //     var table = null;
 
-//     getXml(fetch_id)
-//     .then(xml)
-//     .then(add_uuid => {
-//         if(add_uuid){
-//             return getXml(add_uuid)
-//                 .then(xml)
-
-//         }
-//     })
-//     .catch(error => {
-//         display.innerHTML = "Error" + error.message;
-//     });
-
-
-//     function getXml(uuid){
+//     function getXml(uuid)
+//     {
 //         return fetch(ochre_url + uuid, {redirect:"follow"})
 //         .then(output =>{
 //             if(!output.ok) throw new Error(`Request Invalid ${error}`);
@@ -41,12 +101,12 @@
 //         .then(text_output => {
 //             const parser = new DOMParser();
 //             return parser.parseFromString(text_output, 'text/xml')
-//         })
+//         });
 
 //     }
 
 //     function xml(data){
-//         const property = data.querySelectorAll('property')
+//         const property = data.querySelectorAll('property');
 //         let add_uuid = '';
         
 //         if(!table){
@@ -72,12 +132,11 @@
 //                 tbody.appendChild(row);
 
 //                 if(string.textContent == 'Associated text'){
-//                     const titleLocation = document.getElementById('api-fetch-title');
+//                     const titleLocation = document.getElementById('title');
 //                     titleLocation.innerHTML = `<u><strong>~ ${value.textContent} ~</strong></u><br></br>`;
 //                     add_uuid = value.getAttribute('uuid');
 //                 }
 //             }
-
 
 //         });
 
@@ -85,112 +144,18 @@
 //         return add_uuid;
 //     }
 
+
+//     getXml(fetch_id)
+//       .then(xml)
+//       .then(add_uuid => {
+//           if(add_uuid){
+//               return getXml(add_uuid)
+//                   .then(xml)
+//           }
+//       })
+//       .catch(error => {
+//           display.innerHTML = "Error" + error.message;
+//       });
+
+    
 // })
-
-
-
-
-// //call from ochre api and retrieve results
-// function requestXML(link){
-//     var connect = new XMLHttpRequest();
-//     connect.onreadystatechange = function(){
-//         if(this.readyState == 4 && this.status == 200){
-//             parseXML(this.responseXML);
-//         }
-//     };
-
-//     connect.open("GET", link, true);
-//     connect.send();
-//     //console.log('requestXML -- ok');
-// }
-
-// //extract properties and desired images from ochre xml
-// function parseXML(sourceXML){
-//     var textTitle = sourceXML.getElementsByTagName('identification');
-//     var title_string = document.createTextNode(textTitle[1].textContent);
-//     document.getElementById('title').appendChild(title_string);
-
-//     if(sourceXML.getElementsByTagName('resource')[0].getAttribute("format") == 'image/jpeg'){
-//         var img = document.createElement('img');
-//         var src = link + "&preview";
-//         img.src = src;
-//         document.getElementById('preview').appendChild(img);
-
-//     }
-// };
-
-
-
-
-////////////////////
-
-// Version using XHR
-document.addEventListener('DOMContentLoaded', () => {
-    const uuid = document.getElementById('ochreContainer').getAttribute('data-uuid');
-    const ochre_url = "https://ochre.lib.uchicago.edu/ochre?uuid=";
-    const link = ochre_url + uuid;
-
-    loadXML();
-
-    // Create first function to load the XML
-    function loadXML() {
-      requestXML(link, {redirect: 'follow'});
-      console.log('loadXML -- OK');
-    }
-
-    // Create function to call from OCHRE API and retrieve results
-    // using an XHR object
-    function requestXML(link) {
-      var connect = new XMLHttpRequest();
-      connect.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          parseXML(this.responseXML);  // This passes the XML file we requested to our
-                                       // next function 
-        }
-      };
-      connect.open("GET", link, true);
-      connect.send();
-      console.log('requestXML -- OK');
-    };
-
-    // Create third function to extract properties and desired images from
-    // OCHRE XML
-    function parseXML(sourceXML) {
-
-      // Extract title of item for the webpage
-      var textTitle = sourceXML.getElementsByTagName('identification');
-      var title_string = document.createTextNode(textTitle[1].textContent);
-      document.getElementById('title').appendChild(title_string);
-
-      // Extract properties of item for the wepbage
-      if (sourceXML.getElementsByTagName('property').length > 1) {
-        const properties = sourceXML.querySelectorAll('property');
-        i = 0;
-        properties.forEach(p => {
-          var tr = document.createElement('tr');
-          tr.setAttribute('class', 'ochreTableRows');
-          tr.setAttribute('id', 'row_' + i);
-          document.getElementById('ochreTableBody').appendChild(tr);
-
-          var property = document.createElement('td');
-          property.setAttribute('id', 'property_' + i);
-          property.textContent = p.querySelector('string').textContent;
-          document.getElementById('row_' + i).appendChild(property);
-
-          var value = document.createElement('td');
-          value.setAttribute('id', 'property_value_' + i);
-          value.textContent = p.querySelector('value').textContent;
-          document.getElementById('row_' + i).appendChild(value);
-          i++;
-        });
-      }
-      
-    // Extract preview image of item for the webpage
-      if (sourceXML.getElementsByTagName('resource')[0].getAttribute("format") == 'image/jpeg') {
-        var img = document.createElement('img');
-        var src = link + "&preview";
-        img.src = src;
-        document.getElementById('preview').appendChild(img);
-      }
-    };
-});
